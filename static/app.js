3b9dfcc7ed19e9f536db9ab19f57f36ffcd4564c
Wiktor <65229344+WKR92@users.noreply.github.com>
2021-04-07 17:04:57 +0200

    filter, links, buttons done

--- static/app.js
+++ static/app.js
@@ -1,16 +1,123 @@
-import listOfChannels from './makChannels.js'
+import listOfChannels, {channelsValues} from './makeChannelsBlocks.js'
 
 const mainJSContainer = document.querySelector('.js-content');
-mainJSContainer.innerHTML = listOfChannels.join("");
+mainJSContainer.innerHTML = listOfChannels(channelsValues).join("");
+let isReverseTurnedOn = false
+let isDarkModeOn = false;
 
-// filtry
+// filters:
 
 // input filter
 const titleFilter = document.querySelector('.filter__input')
 titleFilter.addEventListener('input', (event) => {
     let inp = event.target.value;
-    mainJSContainer.innerHTML = listOfChannels
-        .filter(elem => elem.toLowerCase().includes(inp.toLowerCase())).join("");
+
+    if(inp.length > 0){
+        const hints = (channelsValues
+            .filter(elem => elem.title.toLowerCase().includes(inp.toLowerCase())))
+        mainJSContainer.innerHTML = listOfChannels(hints).join("");
+        const readyHints = hints.map(elem => `
+            <option>${elem.title}</option>
+        `);
+        const inpHints = document.getElementById('inpHints');
+        inpHints.innerHTML = readyHints.join("")
+    } else {
+        mainJSContainer.innerHTML = listOfChannels(channelsValues).join("");
+        inpHints.innerHTML = "";
+    }
 })
 
 // radio filters:
+const sortParent = document.querySelector('.sort__options');
+sortParent.addEventListener('input', (event) => {
+
+    switch(event.target.id){
+        case "sort-title":
+            const channelsValuesSortedByTitle = [...channelsValues].sort((a, b) => a.title.localeCompare(b.title))
+            if(isReverseTurnedOn === true){
+                mainJSContainer.innerHTML = listOfChannels(channelsValuesSortedByTitle).reverse().join("")}
+            else {mainJSContainer.innerHTML = listOfChannels(channelsValuesSortedByTitle).join("");}
+            break;
+        case "sort-subscribers":
+            const channelsValuesSortedBySubscribers = [...channelsValues].sort((a, b) => 
+                +a.numOfSubscribers - +b.numOfSubscribers)
+            if(isReverseTurnedOn === true){
+                mainJSContainer.innerHTML = listOfChannels(channelsValuesSortedBySubscribers).reverse().join("");
+            } else {
+                mainJSContainer.innerHTML = listOfChannels(channelsValuesSortedBySubscribers).join("");
+            }
+            break;
+        case "sort-videos":
+            const channelsValuesSortedByVideos = [...channelsValues].sort((a, b) => 
+                +a.numOfVideos - +b.numOfVideos)
+            if(isReverseTurnedOn === true){
+                mainJSContainer.innerHTML = listOfChannels(channelsValuesSortedByVideos).reverse().join("");
+            } else {
+                mainJSContainer.innerHTML = listOfChannels(channelsValuesSortedByVideos).join("");
+            }
+            break;
+        case "sort-views":
+            const channelsValuesSortedByViews = [...channelsValues].sort((a, b) => 
+                +a.numOfViews - +b.numOfViews)
+            if(isReverseTurnedOn === true){
+                mainJSContainer.innerHTML = listOfChannels(channelsValuesSortedByViews).reverse().join("");
+            } else {
+                mainJSContainer.innerHTML = listOfChannels(channelsValuesSortedByViews).join("");
+            }
+            break;
+        default:
+            mainJSContainer.innerHTML = listOfChannels(channelsValues).join("");
+    }
+})
+
+
+
+// buttons:
+
+const clearBtn = document.getElementById('clearBtn');
+const reverseBtn = document.getElementById('reverseBtn');
+const darkModeBtn = document.getElementById('darkModeBtn')
+const lightModeBtn = document.getElementById('lightModeBtn')
+
+clearBtn.addEventListener('click', () => {
+    mainJSContainer.innerHTML = listOfChannels(channelsValues).join("");
+    titleFilter.value = ""
+    const choiceElements = document.querySelectorAll('.choice')
+    choiceElements.forEach(elem => elem.checked = false)  
+})
+
+darkModeBtn.addEventListener('click', () => {
+    const html = document.querySelector('html');
+
+        html.style["background"] = "black";
+        html.style['filter'] = `invert(1)`;
+        darkModeBtn.style["display"] = "none";
+        lightModeBtn.style["display"] = "block";
+})
+
+lightModeBtn.addEventListener('click', () => {
+    const html = document.querySelector('html');
+
+        html.style["background"] = "white";
+        html.style['filter'] = `invert(0)`;
+        lightModeBtn.style["display"] = "none";
+        darkModeBtn.style["display"] = "block";
+})
+
+// order info:
+const orderInfo = document.createElement("p")
+orderInfo.innerHTML = `You are sorting in <span>${isReverseTurnedOn ? 'descending' : 'ascending'}</span> order`;
+orderInfo.classList.add('orderInfo')
+const sortElement = document.querySelector('.sort')
+sortElement.appendChild(orderInfo)
+
+reverseBtn.addEventListener('click', () => {
+    isReverseTurnedOn = !isReverseTurnedOn;
+    orderInfo.innerHTML = `You are sorting in <span>${isReverseTurnedOn ? 'descending' : 'ascending'}</span> order`
+    const choiceElements = document.querySelectorAll('.choice')
+    const checkedLi = Object.values(choiceElements).filter(elem => elem.checked === true)
+    if(checkedLi[0]){
+        checkedLi[0].checked = false;
+        checkedLi[0].click();
+    }
+})
---
+++ static/makeChannelsBlocks.js
@@ -0,0 +1,46 @@
+import channels from './channels.js';
+
+const putCommasBetweenNumbers = (number) => {
+    return number
+        .split(/(?=(?:\d{3})+$)/).join(",")
+}
+
+export const channelsValues = channels.map(elem => ({
+    "title": elem.title,
+    "thumbnail": elem.thumbnails.medium.url,
+    'numOfSubscribers': elem.statistics.subscriberCount.replace(/[,|.|\s]/gi, ""),
+    'numOfVideos': elem.statistics.videoCount.replace(/[,|.|\s]/gi, ""),
+    'numOfViews': elem.statistics.viewCount.replace(/[,|.|\s]/gi, ""),
+    'channelUrl': elem.customUrl
+}))
+
+const listOfChannels = (arr) => {return arr.map(elem => `
+    <a class="channel" id==${elem.title} href=${elem.channelUrl + '?utm_date=' + Date.now()} target="_blank">
+        <div class="channel__thumbnailAndTitle">
+            <img class="thumbnailAndTitle__thumbnail" alt=${elem.title} src=${elem.thumbnail} />
+            <h4 class="thumbnailAndTitle__title">${elem.title}</h4>
+        </div>
+        <div class="channel__info">
+            <div class="info__subscribers">
+                <div class="infoContainer">
+                    <p class="infoTitle">SUBSCRIBERS</p>
+                    <p class="infoNumber">${putCommasBetweenNumbers(elem.numOfSubscribers)}</p>
+                </div>
+            </div>
+            <div class="info__videos">
+                <div class="infoContainer">
+                    <p class="infoTitle">VIDEOS</p>
+                    <p class="infoNumber">${putCommasBetweenNumbers(elem.numOfVideos)}</p>
+                </div>
+            </div>
+            <div class="info__views">
+                <div class="infoContainer">
+                    <p class="infoTitle">VIEWS</p>
+                    <p class="infoNumber">${putCommasBetweenNumbers(elem.numOfViews)}</p>
+                </div>
+            </div>
+        </div>
+    </a>`
+)}
+
+export default listOfChannels;
